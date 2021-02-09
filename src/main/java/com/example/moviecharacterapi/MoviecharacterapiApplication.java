47d78b0c5064eb49954018b3bce9c7dd628c4fc5
Lucas <lucas.rosenorn@outlook.dk>
2021-02-09 12:57:16 +0100

    Initial commit

---
+++ src/main/java/com/example/moviecharacterapi/MoviecharacterapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviecharacterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviecharacterapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviecharacterapiApplication.class, args);
+    }
+
+}
