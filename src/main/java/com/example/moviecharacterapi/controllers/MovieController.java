ec918ae0048805f8e9405c3b68f9cbc02a34f454
GodlikeCPU <dan.tomicic@gmail.com>
2021-02-10 13:27:30 +0100

    feat: MovieController addNewMovie and updateMovie

--- src/main/java/com/example/moviecharacterapi/controllers/MovieController.java
+++ src/main/java/com/example/moviecharacterapi/controllers/MovieController.java
@@ -4,9 +4,7 @@ import com.example.moviecharacterapi.models.Movie;
 import com.example.moviecharacterapi.repositories.MovieRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Set;
@@ -33,6 +31,38 @@ public class MovieController {
             status = HttpStatus.OK;
         }
         return new ResponseEntity<>(movies, status);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovie(@PathVariable long id) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        boolean exists = movieRepository.existsById(id);
+        Movie returnMovie = null;
+        if (exists) {
+            returnMovie = movieRepository.findById(id).get();
+            status = HttpStatus.OK;
+            return new ResponseEntity<>(returnMovie, status);
+        }
+        return new ResponseEntity<>(null, status);
+    }
+
+    @PostMapping
+    public ResponseEntity<Movie> addNewMovie(@RequestBody Movie movie) {
+        HttpStatus status = HttpStatus.CREATED;
+        Movie returnMovie = movieRepository.save(movie);
+        return new ResponseEntity<>(returnMovie, status);
+    }
+
+    @PutMapping
+    public ResponseEntity<Movie> updateMovie(@RequestBody Movie movie) {
+        HttpStatus status = HttpStatus.NO_CONTENT;
+        Movie returnMovie = null;
+        if (movieRepository.existsById(movie.getMovieId())) {
+            returnMovie = movieRepository.save(movie);
+        } else {
+            status = HttpStatus.NOT_FOUND;
+        }
+        return new ResponseEntity<>(returnMovie, status);
     }
 
 }
