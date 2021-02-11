9e4596c6d1bcc056737613b2846deeea2422c21e
GodlikeCPU <dan.tomicic@gmail.com>
2021-02-11 14:30:40 +0100

    feat: JSON-getters for Movie model

--- src/main/java/com/example/moviecharacterapi/models/Movie.java
+++ src/main/java/com/example/moviecharacterapi/models/Movie.java
@@ -1,10 +1,12 @@
 package com.example.moviecharacterapi.models;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 import javax.persistence.*;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Table(name = "movies")
@@ -38,6 +40,34 @@ public class Movie {
     @ManyToOne
     @JoinColumn(name = "franchiseId")
     public Franchise franchise;
+
+    @JsonGetter("characters")
+    public Set<String> getJsonCharacters() {
+        if (characters != null)
+            return characters.stream().map(character -> "/api/v1/characters/" + character.getCharacterId()).collect(Collectors.toSet());
+        return null;
+    }
+
+    @JsonGetter("genres")
+    public Set<String> getJsonGenres() {
+        if (genres != null)
+            return genres.stream().map(genre -> "/api/v1/characters/" + genre.getGenreId()).collect(Collectors.toSet());
+        return null;
+    }
+
+    @JsonGetter("franchise")
+    public String getJsonFranchise() {
+        if (franchise != null)
+            return "/api/v1/franchises/" + franchise.getFranchiseId();
+        return null;
+    }
+
+    @JsonGetter("director")
+    public String getJsonDirector() {
+        if (director != null)
+            return "/api/v1/franchises/" + director.getDirectorId();
+        return null;
+    }
 
     public long getMovieId() {
         return movieId;
